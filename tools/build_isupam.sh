d0c06067022593dea342f7f24924e86a7b087405
shigeyuki.fujishima <shigeyuki.fujishima@gmail.com>
2017-09-18 20:56:30 +0900

    Changed shebang from sh to bash

--- tools/build_isupam.sh
+++ tools/build_isupam.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
 OS_TARGETS="linux darwin freebsd windows"
 
-for os in $OS_TARGETS; do
-  if [ "$os" == "windows" ]; then
-    GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os.exe ./cmd/isupam
+for os in $OS_TARGETS
+do
+  echo "Build isupam for ${os}"
+  if [ "$os" = "windows" ]; then
+    GOOS=$os go build -v -ldflags "-s -w" -o bin/isupam_$os.exe ./cmd/isupam
   else
-    GOOS=$os go build -ldflags "-s -w" -o bin/isupam_$os ./cmd/isupam
+    GOOS=$os go build -v -ldflags "-s -w" -o bin/isupam_$os ./cmd/isupam
   fi
 done
